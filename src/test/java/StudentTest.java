2760fea77b8c5d3263b341ac11101ca68bad26ea
Christian Hall <christian_hall@hotmail.com>
2017-12-01 13:42:12 +0100

    Second commit. Made changes to StudentTest. Tested the three methods testSetAndGetFirstName, testSetAndGetLastName and testSetAndGetAge.Code now pass covered until 22.4%.

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,17 +1,29 @@
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
 import org.junit.Test;
 
-import otherclasses.Classroom;
 import otherclasses.Student;
 
 public class StudentTest {
-	
+	Student s = new Student("Christian", "Hall", 33, 'M');
+	Student s2 = new Student("Gayathri", "Hall", 26, 'F', 3.7, 7.7, 7.7);
+
 	@Test
-	public void testAddANewStudent() {
-	
+	public void testSetAndGetFirstName() {
+		s.setFirstName("Christian");
+		assertEquals(s.getFirstName().equals("Christian"), true);
+		
+	}
+	@Test
+	public void testSetAndGetLastName() {
+		s.setLastName("Hall");
+		assertEquals(s.getLastName().equals("Hall"), true);
+		
+	}
+	@Test
+	public void testSetAndGetAge() {
+		s.setAge(33);
+		assertEquals(s.getAge()==33, true);
 		
 	}
 
